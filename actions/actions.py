30d2c081eddad761854e61a79825b145fd1729e8
Vijay Mamilla <vijay.mamilla@gmail.com>
2023-09-07 22:08:34 -0400

    Created Rest Service with Machine Learning Model.

--- actions/actions.py
+++ actions/actions.py
@@ -20,3 +20,37 @@ class ActionGetWeather(Action):
 
         dispatcher.utter_message(text=response)
         return []
+
+class ActionGetQA(Action):
+    def name(self) -> Text:
+        return "action_immigration_questions"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        question = tracker.latest_message['text']
+
+        response = self.get_immigration_qs(question)
+
+        dispatcher.utter_message(text=response)
+        return []
+
+    @staticmethod
+    def get_immigration_qs(question: str) -> str:
+
+        base_url = "http://127.0.0.1:8000/api/v1/qa"
+        params = {
+            "prompt": question
+        }
+
+        try:
+            print(f"API Request URL: {base_url}?{params}")
+            response = requests.get(base_url, params=params)
+            response.raise_for_status()
+            api_response = response.json()
+            print(f"API Response: {api_response}")
+            return api_response
+        except requests.exceptions.RequestException as e:
+            print(f"API Request Error: {e}")
+            return None  # Return {} in case of exception
--- restservice_app.py
+++ restservice_app.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI, Request
 from typing import Dict
+import pandas as pd
+import numpy as np
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+import json
 
 app = FastAPI()
 
@@ -8,6 +13,50 @@ app = FastAPI()
 async def get_health_check_status() -> Dict:
     """Generate a health check response for the applications."""
     return {"status": "UP", "details": "Application is running normally."}
+
+# Load data, embeddings, and model.  Adjust the paths as needed
+df = pd.read_csv('dataset.csv', low_memory=False, encoding='utf-8')
+embedings_matrix = np.load("embeddings_matrix_v1.npy")
+bert = SentenceTransformer('all-MiniLM-L6-v2')
+
+def get_top(searchQuery, k=1):
+    search_arr = np.array([searchQuery])
+    search_embeddings = bert.encode(search_arr)
+    similarity_matrix = cosine_similarity(search_embeddings, embedings_matrix)
+    top_k_ids = np.argsort(similarity_matrix[0])[-k:][::-1]
+
+    responses = ""
+    for id_ in top_k_ids:
+        row = df.iloc[id_]
+
+        responses = " ".join([row['Content'].strip(), row['Source'].strip()])
+
+    return responses
+
+def get_top_k_matches(searchQuery, k=1):
+    search_arr = np.array([searchQuery])
+    search_embeddings = bert.encode(search_arr)
+    similarity_matrix = cosine_similarity(search_embeddings, embedings_matrix)
+    top_k_ids = np.argsort(similarity_matrix[0])[-k:][::-1]
+
+    responses = []
+    for id_ in top_k_ids:
+        row = df.iloc[id_]
+        answer = {
+            "Title": row['Title'],
+            "Source": row['Source'],
+            "DateOfScrapping": row['DateOfScrapping'],
+            "Content": row['Content']
+        }
+        responses.append(answer)
+
+    return responses
+
+@app.get("/api/v1/qa", summary="Question Answers", description="Returns Answer.")
+async def qa(prompt):
+    result = get_top(prompt)
+    response = result
+    return response
 
 if __name__ == '__main__':
     print('main function called')
